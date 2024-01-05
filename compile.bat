82431da4566092c3cdba51376ff4b12f84ef033b
xy-wu <xy-wu@outlook.com>
2024-01-06 02:10:45 +0900

    clean repo

--- compile.bat
+++
@@ -1,3 +0,0 @@
-%VULKAN_SDK%\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
-%VULKAN_SDK%\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
-pause
