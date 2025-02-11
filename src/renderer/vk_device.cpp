b2c96e6721d1c665d56fa5208eb2d280aecb2c1e
wxy <wxy@g.ecc.u-tokyo.ac.jp>
2025-02-11 16:34:26 +0900

    Simplify compile process: auto download glfw, glm

--- src/renderer/vk_device.cpp
+++ src/renderer/vk_device.cpp
@@ -67,7 +67,7 @@ void XevDevice::createInstance() {
 
   VkApplicationInfo appInfo  = {};
   appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-  appInfo.pApplicationName   = "LittleVulkanEngine App";
+  appInfo.pApplicationName   = "Xenon Engine Vulkan App";
   appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
   appInfo.pEngineName        = "No Engine";
   appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
--- src/renderer/vk_window.hpp
+++ src/renderer/vk_window.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #define GLFW_INCLUDE_VULKAN
-
 #include <GLFW/glfw3.h>
 
 #include <glm/vec2.hpp>
+
 #include <string>
 
 namespace xev {
