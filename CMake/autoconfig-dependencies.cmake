a388bac9c3270e45b297874f2bca66a521beaf9c
wxy <xy-wu@outlook.com>
2025-04-05 21:00:38 +0900

    fix ci

--- CMake/autoconfig-dependencies.cmake
+++ CMake/autoconfig-dependencies.cmake
@@ -10,15 +10,32 @@ endif ()
 # Auto-setup dependencies
 include(FetchContent)
 
-FetchContent_Declare(glfw GIT_REPOSITORY https://github.com/glfw/glfw.git GIT_TAG master)
-set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(glfw)
+# Check if GLFW is already installed
+find_package(glfw3 QUIET)
+
+if(NOT glfw3_FOUND)
+    message(STATUS "GLFW not found, fetching from GitHub...")
+    FetchContent_Declare(glfw GIT_REPOSITORY https://github.com/glfw/glfw.git GIT_TAG master)
+    set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+    # set(GLFW_BUILD_WAYLAND OFF CACHE BOOL "Disable Wayland support in GLFW" FORCE)
+    FetchContent_MakeAvailable(glfw)
+else()
+    message(STATUS "GLFW already installed. Using version: ${glfw3_VERSION}")
+endif()
 
 message(STATUS "GLFW include dir: ${glfw_SOURCE_DIR}/include")
 
-# Math lib: GLM
-FetchContent_Declare(glm GIT_REPOSITORY	https://github.com/g-truc/glm.git GIT_TAG master)
-FetchContent_MakeAvailable(glm)
+# Check if GLM is already installed
+find_package(glm QUIET)
+
+if(NOT glm_FOUND)
+    message(STATUS "GLM not found, fetching from GitHub...")
+    FetchContent_Declare(glm GIT_REPOSITORY https://github.com/g-truc/glm.git GIT_TAG master)
+    FetchContent_MakeAvailable(glm)
+else()
+    message(STATUS "GLM already installed. Using version: ${glm_VERSION}")
+endif()
+
 message(STATUS "GLM include dir: ${glm_SOURCE_DIR}/glm")
