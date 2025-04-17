5aa79f3f5723809627b4ba765995590c7ebb15b8
wxy <xy-wu@outlook.com>
2025-04-17 17:29:10 +0900

    Add Imgui support in CMake configuration and improve fetching logic

--- CMake/find-imgui.cmake
+++ CMake/find-imgui.cmake
@@ -4,22 +4,29 @@ find_package(imgui QUIET)
 
 if (NOT imgui_FOUND)
     message(STATUS "Imgui not found, fetching from GitHub...")
-    FetchContent_Declare(imgui
+    FetchContent_Declare(imgui_src
             URL https://github.com/ocornut/imgui/archive/refs/tags/v1.91.9b.tar.gz
-            EXCLUDE_FROM_ALL
+            #            EXCLUDE_FROM_ALL
     )
-    FetchContent_MakeAvailable(imgui)
+    FetchContent_MakeAvailable(imgui_src)
+    message(STATUS "Imgui src dir: ${imgui_src_SOURCE_DIR}")
+
+    add_library(imgui
+            ${imgui_src_SOURCE_DIR}/imgui.cpp
+            ${imgui_src_SOURCE_DIR}/imgui_draw.cpp
+            ${imgui_src_SOURCE_DIR}/imgui_tables.cpp
+            ${imgui_src_SOURCE_DIR}/imgui_widgets.cpp
+            ${imgui_src_SOURCE_DIR}/backends/imgui_impl_vulkan.cpp
+            ${imgui_src_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+    )
+    target_include_directories(imgui
+            PUBLIC ${imgui_src_SOURCE_DIR}
+            PUBLIC ${imgui_src_SOURCE_DIR}/backends
+    )
+    message(STATUS "Imgui setup complete.")
 else ()
     message(STATUS "Imgui already installed. Using version: ${imgui_VERSION}")
 endif ()
 
-message(STATUS "Imgui src dir: ${imgui_SOURCE_DIR}")
 
-#add_library(imgui
-#        ${imgui_external_SOURCE_DIR}/imgui.cpp
-#        ${imgui_external_SOURCE_DIR}/imgui_draw.cpp
-#        ${imgui_external_SOURCE_DIR}/imgui_tables.cpp
-#        ${imgui_external_SOURCE_DIR}/imgui_widgets.cpp
-#        ${imgui_external_SOURCE_DIR}/backends/imgui_impl_allegro5.cpp
-#)
-#target_include_directories(imgui PUBLIC ${imgui_external_SOURCE_DIR})
+
